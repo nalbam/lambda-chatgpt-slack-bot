03d5c71e7cbc70d6880a9eb0a0310c8152782058
nalbam <me@nalbam.com>
2023-02-28 17:22:31 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # lambda-chatgpt-slack-bot
+
+* <https://pypi.org/project/revChatGPT/>
+
+## Install
+
+```bash
+$ brew install python@3.9
+
+$ npm install -g serverless
+
+$ sls plugin install -n serverless-python-requirements
+$ sls plugin install -n serverless-dotenv-plugin
+
+$ pip3 install --upgrade -r requirements.txt
+```
+
+## Setup
+
+Setup a Slack app by following the guide at https://slack.dev/bolt-js/tutorial/getting-started
+
+Set scopes to Bot Token Scopes in OAuth & Permission:
+
+```
+app_mentions:read
+channels:join
+chat:write
+im:history
+im:read
+im:writ
+```
+
+Set scopes in Event Subscriptions - Subscribe to bot events
+
+```
+app_mention
+message.im
+```
+
+## Deployment
+
+In order to deploy the example, you need to run the following command:
+
+```bash
+$ cp .env.example .env
+
+$ sls deploy
+```
+
+## Slack Test
+
+```bash
+curl -X POST -H "Content-Type: application/json" \
+-d " \
+{ \
+    \"token\": \"Jhj5dZrVaK7ZwHHjRyZWjbDl\", \
+    \"challenge\": \"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P\", \
+    \"type\": \"url_verification\" \
+}" \
+https://xxxx.execute-api.us-east-1.amazonaws.com/dev/slack/events
+```
---
+++ handler.py
@@ -0,0 +1,141 @@
+import boto3
+import json
+import os
+
+from revChatGPT.V1 import Chatbot
+
+from slack_bolt import App, Say
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+# Set up ChatGPT API credentials
+CHATGPT_ACCESS_TOKEN = os.environ["CHATGPT_ACCESS_TOKEN"]
+CHATGPT_CURSOR = os.environ.get("CHATGPT_CURSOR", ":robot_face:")
+
+# Initialize ChatGPT
+chatbot = Chatbot(config={"access_token": CHATGPT_ACCESS_TOKEN})
+
+# Set up Slack API credentials
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+
+# Initialize Slack app
+app = App(
+    token=SLACK_BOT_TOKEN,
+    signing_secret=SLACK_SIGNING_SECRET,
+    process_before_response=True,
+)
+
+# Keep track of conversation history by thread
+DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME", "chatgpt-slack-bot-context")
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
+
+
+def get_context(id):
+    item = table.get_item(Key={"id": id}).get("Item")
+    return (
+        (item["conversation_id"], item["parent_id"], item["prompt"])
+        if item
+        else ("", "", "")
+    )
+
+
+def put_context(id, conversation_id="", parent_id="", prompt=""):
+    table.put_item(
+        Item={
+            "id": id,
+            "conversation_id": conversation_id,
+            "parent_id": parent_id,
+            "prompt": prompt,
+        }
+    )
+
+
+# Update the message in Slack
+def chat_update(channel, message, latest_ts):
+    print("chat_update: {}".format(message))
+    app.client.chat_update(
+        channel=channel,
+        text=message,
+        ts=latest_ts,
+    )
+
+
+# Handle the chatgpt conversation
+def conversation(thread_ts, prompt, channel, say: Say):
+    print(thread_ts, prompt)
+
+    # Keep track of the latest message timestamp
+    result = say(text=CHATGPT_CURSOR, thread_ts=thread_ts)
+    latest_ts = result["ts"]
+
+    conversation_id, parent_id, _ = get_context(thread_ts)
+
+    try:
+        # Send the prompt to ChatGPT
+        for response in chatbot.ask(prompt, conversation_id, parent_id):
+            message = response["message"]
+
+            chat_update(channel, message + " " + CHATGPT_CURSOR, latest_ts)
+
+            conversation_id, parent_id = (
+                response["conversation_id"],
+                response["parent_id"],
+            )
+
+        put_context(thread_ts, conversation_id, parent_id)
+    except Exception as e:
+        print(thread_ts, "Error handling message: {}".format(e))
+        message = "Sorry, I could not process your request.\nhttps://status.openai.com"
+
+    if message != "":
+        chat_update(channel, message, latest_ts)
+
+
+# Handle the app_mention event
+@app.event("app_mention")
+def handle_app_mentions(body: dict, say: Say):
+    print("handle_app_mentions: {}".format(body))
+
+    event = body["event"]
+
+    thread_ts = event["thread_ts"] if "thread_ts" in event else event["ts"]
+    prompt = event["text"].split("<@")[1].split(">")[1].strip()
+
+    # # Check if this is a message from the bot itself, or if it doesn't mention the bot
+    # if "bot_id" in event or f"<@{app.client.users_info(user=SLACK_BOT_TOKEN)['user']['id']}>" not in text:
+    #     return
+
+    conversation(thread_ts, prompt, event["channel"], say)
+
+
+def lambda_handler(event, context):
+    body = json.loads(event["body"])
+
+    if "challenge" in body:
+        # Respond to the Slack Event Subscription Challenge
+        return {
+            "statusCode": 200,
+            "headers": {"Content-type": "application/json"},
+            "body": json.dumps({"challenge": body["challenge"]}),
+        }
+
+    print("lambda_handler: {}".format(body))
+
+    # Duplicate execution prevention
+    token = body["event"]["client_msg_id"]
+    _, _, prompt = get_context(token)
+    if prompt == "":
+        put_context(token, "", "", body["event"]["text"])
+    else:
+        return {
+            "statusCode": 200,
+            "headers": {"Content-type": "application/json"},
+            "body": json.dumps({"status": "Success"}),
+        }
+
+    # Handle the event
+    slack_handler = SlackRequestHandler(app=app)
+    return slack_handler.handle(event, context)
